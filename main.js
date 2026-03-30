404ea27babbdac432a19aeae86fd3e21ab1dc945
JewelVuyanzi <jimbamba@kabarak.ac.ke>
2026-03-30 23:38:35 +0300

    mainjs file

---
+++ main.js
@@ -0,0 +1,12 @@
+function myFunction(){
+            alert("Alert!!!");
+        }
+function myNew(){
+    var txt;
+    if(confirm("Are you sure you want to delete?"))
+        {
+            txt ="YES";
+        }else {
+            txt ="NO";
+        }
+    }
